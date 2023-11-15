b0d4d7f3901edcb03868888ceea9de6ffc48721e
KOO612 <ksm99007@gmail.com>
2023-11-16 00:03:07 +0900

    미들웨어

--- app.js
+++ app.js
@@ -4,9 +4,25 @@ const path = require('path');
 const app = express();
 app.set('port', process.env.PORT || 7000);
 
-app.get('/', (req, res) => {
-  // res.send('hello express');
-  res.sendFile(path.join(__dirname, '/index.html'));
+app.use((req, res, next) => {
+  console.log('모든 요청에 다 실행됩니다');
+  next();
+});
+
+app.get(
+  '/',
+  (req, res, next) => {
+    console.log('get 요청에 실행');
+    next();
+  },
+  (req, res) => {
+    throw new Error('에러는 에러 처리 미들웨어');
+  }
+);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(err.message);
 });
 
 app.listen(app.get('port'), () => {
