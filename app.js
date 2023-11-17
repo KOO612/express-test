e52f18e1ac8d902a96b9ce5d23008ef8b6a31513
KOO612 <ksm99007@gmail.com>
2023-11-17 12:07:54 +0900

    11/17 미들웨어

--- app.js
+++ app.js
@@ -8,15 +8,25 @@ const path = require('path');
 dotenv.config();
 const app = express();
 app.set('port', process.env.PORT || 7000);
+// const port = 7000; 하드 코딩과 다르게 동적 설정
+// process.env 객체에 PORT 속성을 넣어서 사용
 
 app.use(morgan('dev'));
+// morgan 미들웨어
+
 app.use('/', express.static(path.join(__dirname, 'public')));
+// static 미들웨어
+
 app.use(express.json());
 app.use(
   express.urlencoded({
     extended: false,
   })
 );
+// body-parser 사용
+// req.body로 변경해줌
+
+app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(
   session({
     resave: false,
