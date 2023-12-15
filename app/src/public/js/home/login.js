e7dbbf1c9a294798144609f657fbfc88823b973c
KOO612 <ksm99007@gmail.com>
2023-12-15 17:21:46 +0900

    defer 클릭 이벤트

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,1 +1,14 @@
-console.log('hello');
+const id = document.querySelector('#id'),
+  pass = document.querySelector('#pass'),
+  loginBtn = document.querySelector('#loginBtn');
+
+loginBtn.addEventListener('click', login);
+
+function login() {
+  const req = {
+    id: id.value,
+    pass: pass.value,
+  };
+
+  console.log(req);
+}
