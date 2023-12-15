75d71e8fdac4e75d1c16cb5541408a555ea9f1ac
KOO612 <ksm99007@gmail.com>
2023-12-15 16:23:17 +0900

    정적 파일 적용

--- app/app.js
+++ app/app.js
@@ -6,8 +6,9 @@ const app = express();
 const home = require('./src/routes/home');
 
 //앱 세팅
-app.set('views', './views');
+app.set('views', './src/views');
 app.set('view engine', 'ejs');
+app.use(express.static(`${__dirname}/src/public`));
 
 // use -> 미들웨어를 등록해주는 메소드
 app.use('/', home);
