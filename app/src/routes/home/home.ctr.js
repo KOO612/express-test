570baf150b4f314ce89849b234ad9360fa9759ba
KOO612 <ksm99007@gmail.com>
2023-12-18 11:36:20 +0900

    로그인 인증 기능 추가

--- app/src/routes/home/home.ctr.js
+++ app/src/routes/home/home.ctr.js
@@ -1,12 +1,38 @@
-const home = (req, res) => {
-  res.render('home/index');
+const users = {
+  id: ['qwer', '나개발', '김팀장'],
+  pass: ['1234', '1234', 'qwer'],
 };
 
-const login = (req, res) => {
-  res.render('home/login');
+const output = {
+  home: (req, res) => {
+    res.render('home/index');
+  },
+  login: (req, res) => {
+    res.render('home/login');
+  },
+};
+
+const process = {
+  login: (req, res) => {
+    const id = req.body.id,
+      pass = req.body.pass;
+
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if (users.pass[idx] === pass) {
+        return res.json({
+          success: true,
+        });
+      }
+    }
+    return res.json({
+      success: false,
+      msg: '로그인 실패',
+    });
+  },
 };
 
 module.exports = {
-  home,
-  login,
+  output,
+  process,
 };
